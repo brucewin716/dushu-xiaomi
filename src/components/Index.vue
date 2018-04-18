837385638bee38a8a58d58bd5d803335b301756f
brucewin716 <brucewin716@gmail.com>
2018-04-18 19:48:23 +0800

    aa

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <mt-header>
+    <router-link to="/" slot="left">
+      <mt-button icon='back'></mt-button>
+    </router-link>
+    <div class='mint-header-button is-left'>
+     jfdljfkldfjkdklfkdlkf
+    </div>
+    <mt-button slot="right"><i class="fa fa-user-o fa-lg"></i></mt-button>
+</mt-header>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
